35429562875e34c993cc3683a8632da83f4491f2
kdelieva <k.delieva21@gmail.com>
2026-05-06 20:06:59 +0300

    New sloved excercises

---
+++ 01. The Complete JavaScript Course/01. JavaScript Fundamentals 01/12.strings-and-template-literals.js
@@ -0,0 +1,15 @@
+const firstName = "Jonas";
+const job = "teacher";
+const birthYear = 1991;
+const year = 2037;
+
+//Type cohersion
+
+const jonas = "I'm " + firstName + ', a ' + (year - birthYear) + ' year old ' + job + "!";
+
+console.log(jonas);
+console.log("------------");
+//Template Literals added in ES6
+
+const jonasNew = `I'm ${firstName}, a ${year - birthYear} year old ${job}!`;
+console.log(jonasNew);
