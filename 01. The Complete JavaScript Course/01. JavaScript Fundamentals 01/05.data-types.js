9007f8dd40e29ad04d49cf4da98619a6cf60ca52
kdelieva <k.delieva21@gmail.com>
2026-05-06 09:24:25 +0300

    Finish the lecture for Data Types

--- 01. The Complete JavaScript Course/01. JavaScript Fundamentals 01/05.data-types.js
+++ 01. The Complete JavaScript Course/01. JavaScript Fundamentals 01/05.data-types.js
@@ -9,3 +9,39 @@
 // - Undefined - example - let age;
 // - Null - empty value as Undefined
 // - BigInt - for very bigger numbers
+
+//One line-comment
+
+/* Multi
+line
+comment*/
+
+console.log(true);
+console.log('------');
+let javascriptIsFun = true;
+console.log(javascriptIsFun);
+
+//typeof Operator show the type of value
+console.log(typeof true);
+console.log('------');
+console.log(typeof javascriptIsFun);
+console.log(typeof 23);
+console.log(typeof "Jonas");
+
+//Dynaming Typing in action
+javascriptIsFun = 'YES!';
+console.log('------');
+console.log(typeof javascriptIsFun);
+
+let year;
+console.log(year);
+console.log('------');
+console.log(typeof year);
+
+year = 1991;
+console.log(year);
+console.log(typeof year);
+
+
+//Bug is that typeof null is object, null is not object!
+console.log(typeof null);
