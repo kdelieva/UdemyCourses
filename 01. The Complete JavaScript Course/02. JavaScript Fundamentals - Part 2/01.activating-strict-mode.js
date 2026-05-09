897a66bcfbc4bcf361c9e18e0fbacf765d37c4ab
kdelieva <k.delieva21@gmail.com>
2026-05-09 22:19:16 +0300

    Start learning JS Fundamentals Part 2 ^_^

---
+++ 01. The Complete JavaScript Course/02. JavaScript Fundamentals - Part 2/01.activating-strict-mode.js
@@ -0,0 +1,15 @@
+'use strict';
+
+//using strict mode help us to avouid bugs
+
+//Example
+let hasDriversLicense = false;
+const passTest = true;
+
+if(passTest) hasDriversLicense = true;
+
+if(hasDriversLicense) console.log('I can drive. :D');
+
+// const interface = 'Audio'; //interface is reserved word and we have got syntax error only if strict mode is turn on
+//Other example
+// const private = 534;
