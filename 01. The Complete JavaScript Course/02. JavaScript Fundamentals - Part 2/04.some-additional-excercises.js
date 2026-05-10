59c4b47cef1cc9080dccb9713ab90cfe3c977297
kdelieva <k.delieva21@gmail.com>
2026-05-10 21:55:32 +0300

    New solved excercises

--- 01. The Complete JavaScript Course/02. JavaScript Fundamentals - Part 2/04.some-additional-excercises.js
+++ 01. The Complete JavaScript Course/02. JavaScript Fundamentals - Part 2/04.some-additional-excercises.js
@@ -48,30 +48,102 @@ console.log("👉Excercise 03: Age Filter👈");
 // Функцията трябва да връща true, ако човекът е в трудоспособна възраст, и false, ако не е.
 // Извикай функцията няколко пъти с различни числа (напр. 15, 25, 70) и изпиши резултатите в конзолата.
 
+const checkWorkEligibility = function (age) {
+    const result = age >= 18 && age <= 65;
+    return result;
+}
 
+console.log(`Is the human can work? The answer is: ${checkWorkEligibility(15)}, because the age is 15.`);
+console.log(`Is the human can work? The answer is: ${checkWorkEligibility(25)}, because the age is 25.`);
+console.log(`Is the human can work? The answer is: ${checkWorkEligibility(70)}, because the age is 70.`);
 
-
-
-
-
-
-
-
+console.log("-----------------------------------");
+console.log("👉Excercise 04: Name Corrector👈");
 // Коректор на имена: Напиши функция, която приема име (напр. "jOnAs" или "pEtEr"). Функцията трябва да връща името в правилен формат — първата буква главна, останалите малки ("Jonas").
 
+function nameCorrector(firstName) {
+    const toLowerLetters = firstName.toLowerCase();
+    const toArray = toLowerLetters.split(''); 
+    toArray[0] = toArray[0].toUpperCase();
+    
+    return toArray.join('');
+}
+
+console.log(nameCorrector("jOnAs"));
+console.log(nameCorrector("pEtEr"));
+
+console.log("-----------------------------------");
+console.log("👉Excercise 05: Toll Tax👈");
 // Пътна такса (Tol-Tax): Създай функция, която изчислява такса за преминаване по мост. Параметри: vehicleType ("car", "truck", "bike") и isHoliday (boolean).
 
 // Кола: 5 лв.
-
 // Камион: 15 лв.
-
 // Мотор: 2 лв.
-
 // Ако е празник (isHoliday е true), всички такси се удвояват.
 
+function tollTaxCalculator(vehicleType, isHoliday) {
+    let price = 0;
+
+    switch(vehicleType) {
+        case 'car': price = 5; break;
+        case 'truck': price = 15; break;
+        case 'motorcycle': price = 2; break;
+    }
+
+    return isHoliday ? price * 2 : price;
+}
+
+const dataOne = tollTaxCalculator('truck', false);
+const dataTwo = tollTaxCalculator('motorcycle', true);
+const dataThree = tollTaxCalculator('car', true);
+
+console.log(`The Toll Tax is: ${dataOne}`);
+console.log(`The Toll Tax is: ${dataTwo}`);
+console.log(`The Toll Tax is: ${dataThree}`);
+
+console.log("-----------------------------------");
+console.log("👉Excercise 06: Smart Watch👈");
 // Умен будилник: Напиши функция, която приема day (стринг) и isAsleep (boolean). Ако е "Saturday" или "Sunday" и isAsleep е true, върни "Keep sleeping". Ако е делничен ден и е true, върни "Alarm ringing!".
 
+const smartWatch = function (day, isAsleep) {
+    let weekendBoolean = false;
+    let workBoolean = false;
+    switch(day) {
+        case 'Saturday':
+        case "Sunday":
+            weekendBoolean = true;
+        break;
+        case 'Monday':
+        case 'Tuesday':
+        case 'Wednesday':
+        case 'Thursday':
+        case 'Friday':
+            workBoolean = true;
+        break;
+    }
+
+    if(workBoolean && isAsleep) {
+        return "Alarm ringing";
+    }else if(weekendBoolean && isAsleep) {
+        return "Keep sleeping";
+    }else {
+        return 'Alarm is OFF';
+    }
+}
+
+const firstStatus = smartWatch('Wednesday', true);
+const secondStatus = smartWatch('Sunday', false);
+console.log(firstStatus);
+console.log(secondStatus);
+
+
+console.log("-----------------------------------");
+console.log("👉Excercise 07: Smart Watch👈");
 // Проверка на наличност в склад: Напиши функция, която приема orderedQuantity и stockQuantity. Ако поръчаното е повече от наличното, върни колко бройки не достигат. Ако е налично, върни "Order processed".
+
+
+
+
 
 // Калкулатор на разход за гориво: Напиши функционален израз, който приема distance (км), consumption (разход на 100 км) и pricePerLiter. Върни общата сума за пътуването.
 
