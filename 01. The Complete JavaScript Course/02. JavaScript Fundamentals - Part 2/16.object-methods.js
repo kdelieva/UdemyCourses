17688d4fbb949a48e95935759597004d41af510c
kdelieva <k.delieva21@gmail.com>
2026-05-12 22:00:28 +0300

    Update

--- 01. The Complete JavaScript Course/02. JavaScript Fundamentals - Part 2/16.object-methods.js
+++ 01. The Complete JavaScript Course/02. JavaScript Fundamentals - Part 2/16.object-methods.js
@@ -3,30 +3,30 @@
 //Objects Methods
 
 const jonas = {
-    firstName: 'Jonas',
-    lastName: "Schmedtmann",
-    birthYear: 1991,
-    job: "teacher",
-    friends: ['Michael', 'Peter', 'Steven'],
-    hasDriversLicense: true,
+  firstName: "Jonas",
+  lastName: "Schmedtmann",
+  birthYear: 1991,
+  job: "teacher",
+  friends: ["Michael", "Peter", "Steven"],
+  hasDriversLicense: true,
 
-    // calcAge: function(birthYear) { //we MUST USED function EXPRESSION!! Not Declaration...
-    //     return 2037 - birthYear;
-    // }
+  // calcAge: function(birthYear) { //we MUST USED function EXPRESSION!! Not Declaration...
+  //     return 2037 - birthYear;
+  // }
 
-    // calcAge: function() {
-    //     // console.log(this);
-    //     return 2037 - this.birthYear;
-    // }
+  // calcAge: function() {
+  //     // console.log(this);
+  //     return 2037 - this.birthYear;
+  // }
 
-    calcAge: function() {
-        this.age = 2037 - this.birthYear;
-        return this.age;
-    },
+  calcAge: function () {
+    this.age = 2037 - this.birthYear;
+    return this.age;
+  },
 
-    getSummary: function() {
-        return `${this.firstName} is a ${this.calcAge()}-year old ${this.job}, and he has ${this.hasDriversLicense ? "a": "no"} driver's license.`;
-    }
+  getSummary: function () {
+    return `${this.firstName} is a ${this.calcAge()}-year old ${this.job}, and he has ${this.hasDriversLicense ? "a" : "no"} driver's license.`;
+  },
 };
 
 // console.log(jonas.calcAge(jonas.birthYear));
