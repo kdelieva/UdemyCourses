0036942379ffd0c345178fa09e93748c71b61ea9
kdelieva <k.delieva21@gmail.com>
2026-05-14 15:39:03 +0300

    New solved Excercises

--- 01. The Complete JavaScript Course/02. JavaScript Fundamentals - Part 2/18.some-additional-excercises.js
+++ 01. The Complete JavaScript Course/02. JavaScript Fundamentals - Part 2/18.some-additional-excercises.js
@@ -72,3 +72,58 @@ console.log("👉Excercise 04: Valid Property Checker👈");
 // Задача 4: Проверка за валидно свойство
 // Напиши функция updateSettings, която приема обект settings и стринг key. Ако ключът key съществува в обекта, промени стойността му на "modified". Ако не съществува, го добави със стойност "new".
 
+
+const objSettings = {};
+
+function updateSettings(settings, key) {
+  if(settings[key] !== undefined) {
+     settings[key] = "modified";
+    return settings;
+  }else {
+    settings[key] = 'new';
+    return settings;
+  }
+}
+
+const objFunction = updateSettings(objSettings, "test");
+console.log(objFunction);
+
+
+console.log("---------------------------------------")
+console.log("👉Excercise 05: Tasks Manager👈");
+// Мениджър на задачи 📋
+// Представи си, че имаш обект, който следи твоите задачи за деня.
+
+// Условие:
+// Създай обект todoList:
+
+// Свойство tasks: Масив от стрингове (напр. ["Learn Objects", "Drink Coffee"]).
+
+// Метод addTask: Приема име на задача (стринг) и я добавя в масива tasks.
+
+// Метод countTasks: Връща съобщение: "You have [брой] tasks to do!".
+
+// Какво упражняваме тук?
+
+// Достъп до масив чрез this.tasks.
+
+// Използване на метода .push() върху масив, който е част от обект.
+
+const toDoList = {
+  tasks: ["Learn Objects", "Drink Coffee"],
+
+  addTask: function(taskName) {
+    const arr = this.tasks;
+    arr.push(taskName)
+    return toDoList;
+  },
+
+  countTasks: function() {
+    const numberTasks = (this.tasks).length;
+    return numberTasks;
+  }
+}
+
+toDoList.addTask("Learn Functions");
+const tasksTypes = toDoList["tasks"];
+console.log(`We have got total Tasks: ${toDoList.countTasks()}, and they are:`, toDoList.tasks);
