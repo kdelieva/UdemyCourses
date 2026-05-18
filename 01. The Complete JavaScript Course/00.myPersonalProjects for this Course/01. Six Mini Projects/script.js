6506c29419edb12db5b1afc71ae6e29db4e47b0f
kdelieva <k.delieva21@gmail.com>
2026-05-18 13:50:20 +0300

    AddSome Changes

--- 01. The Complete JavaScript Course/00.myPersonalProjects for this Course/01. Six Mini Projects/script.js
+++ 01. The Complete JavaScript Course/00.myPersonalProjects for this Course/01. Six Mini Projects/script.js
@@ -1,69 +1,14 @@
 "use strict";
+const bill = document.querySelector("input");
+const percentTip = document.querySelector(".tip-bold");
+const selectedButton = document.querySelector('.selected');
 
+bill.addEventListener("input", () => {
+    const tip = Number(bill.value);
+    percentTip.innerHTML = `${tip.toFixed(2)} eur`;
+});
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-// 
-// const bill = document.querySelector("#bill");
-// const result = document.querySelector(".tip-bold"); 
-// const buttons = document.querySelectorAll('.tip-percent');
-
-// 
-// function getSelectedTipPercent() {
-//     const selectedButton = document.querySelector('.tip-percent.selected');
-//     if (selectedButton) {
-//         return Number(selectedButton.value); 
-//     }
-//     return 0;
-// }
-
-// function tipCalc(billValue, percentValue) {
-//     return billValue * (percentValue / 100);
-// }
-
-//
-// function updateUI() {
-//     const billValue = Number(bill.value);
-//     const percentValue = getSelectedTipPercent();
-//     const finalTip = tipCalc(billValue, percentValue);
-    
-// 
-//     result.textContent = `${finalTip.toFixed(2)} eur`;
-// }
-
-//
-// bill.addEventListener("input", () => {
-//     updateUI(); 
-// });
-
-// buttons.forEach(button => {
-//     button.addEventListener('click', function(e) {
-//         e.preventDefault(); // Спира презареждането на формата
-        
-//         const currentSelected = document.querySelector('.selected');
-//         if (currentSelected) {
-//             currentSelected.classList.remove('selected');
-//         }
-
-//         this.classList.add('selected');
-
-// 
-//         updateUI(); 
-//     });
-// });
+selectedButton.addEventListener("click", () => {
+    const tip = Number(bill.value * (tipValue / 100));
+    percentTip.innerHTML = `${tip.toFixed(2)} eur`;
+});
