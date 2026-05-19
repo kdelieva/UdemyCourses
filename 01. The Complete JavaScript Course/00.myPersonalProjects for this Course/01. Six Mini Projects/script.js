2b6a4cc7cd909a89719b28b0d4d42fe8da8b9b46
kdelieva <k.delieva21@gmail.com>
2026-05-19 21:22:17 +0300

    add new materials that i learned and trained

--- 01. The Complete JavaScript Course/00.myPersonalProjects for this Course/01. Six Mini Projects/script.js
+++ 01. The Complete JavaScript Course/00.myPersonalProjects for this Course/01. Six Mini Projects/script.js
@@ -1,14 +1,16 @@
 "use strict";
 const bill = document.querySelector("input");
 const percentTip = document.querySelector(".tip-bold");
-const selectedButton = document.querySelector('.selected');
+const selectedButton = document.querySelector(".selected");
+const tipPerc = document.querySelector(".tip-inside");
 
 bill.addEventListener("input", () => {
-    const tip = Number(bill.value);
-    percentTip.innerHTML = `${tip.toFixed(2)} eur`;
+  const tip = Number(bill.value);
+  percentTip.innerHTML = `${tip.toFixed(2)} eur`;
 });
 
 selectedButton.addEventListener("click", () => {
-    const tip = Number(bill.value * (tipValue / 100));
-    percentTip.innerHTML = `${tip.toFixed(2)} eur`;
+  const tip = Number(bill.value * (Number(selectedButton.value) / 100));
+  percentTip.innerHTML = `${tip.toFixed(2)} eur`;
+  tipPerc.innerHTML = `${Number(selectedButton.value)}%`;
 });
---
+++ 01. The Complete JavaScript Course/05. JS in the Browser - DOM and Events - PROJECT/script.js
@@ -0,0 +1,5 @@
+'use strict';
+
+document.querySelector(".message");
+
+console.log(document.querySelector(".message").textContent);
